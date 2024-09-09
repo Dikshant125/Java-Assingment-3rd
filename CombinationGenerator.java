9261332e22b34c169ad1e9067fef8e6834287852
Dikshant125 <dikshantgoswami441@gmail.com>
2024-09-09 14:53:01 +0530

    Add files via upload

---
+++ CombinationGenerator.java
@@ -0,0 +1,45 @@
+/***
+ * Question: Write a Java program that generates all valid combinations of the string pairs of
+ * parentheses. Each combination should be unique and well-formed.
+ * Owner Name: Dikshant Goswami
+ * Date: 9-9-2024
+ */
+
+import java.util.Scanner;
+
+class index {
+
+    // Swap elements at index i and j in the array
+    public static void swap(char[] array, int start, int end) {
+        char temporary = array[start];
+        array[start] = array[end];
+        array[end] = temporary;
+    }
+
+    // Generate all permutations of the given character array
+    public static void generateCombinations(char[] inputChar, int start, int length) {
+        printCombination(inputChar, start);
+
+        for (int initial = start; initial < length; initial++) {
+            swap(inputChar, initial, start);
+            generateCombinations(inputChar, start + 1, length);
+            swap(inputChar, initial, start);
+        }
+    }
+
+    // Print the current permutation up to the specified end index
+    public static void printCombination(char[] chars, int end) {
+        for (int initial = 0; initial < end; initial++) {
+            System.out.print(chars[initial]);
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        String input;
+        System.out.println(Constants.ENTER_STRING);
+        input = userInput.next();
+        char[] inputChar = input.toCharArray();
+        generateCombinations(inputChar, 0, inputChar.length);
+        }
+}
